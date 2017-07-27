6d3a42422e4a441a26ed2712dbdde7f81dd04f9e
Eduardo Moreno <ejmm320@gmail.com>
2017-07-27 17:35:17 -0400

    Postgresql database creation using jsonb format to store the payload

---
+++ app/models/weather_data.rb
@@ -0,0 +1,3 @@
+class WeatherData < ApplicationRecord
+  
+end
---
+++ db/migrate/20170727211403_create_weather_data.rb
@@ -0,0 +1,8 @@
+class CreateWeatherData < ActiveRecord::Migration[5.1]
+  def change
+    create_table :weather_data do |t|
+      t.jsonb :payload, null: false, default: {}
+    end
+    add_index :weather_data, :payload, using: :gin
+  end
+end
